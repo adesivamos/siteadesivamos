21a19d8b6ee6f46e4650541f4d0c46e29e9f9dc8
Joelson Dias <joelsondiasti@outlook.com>
2021-07-12 13:27:32 -0300

    feat: Add Facebook Domain Verification

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,700&display=swap"
           rel="stylesheet"
         />
+        <meta name="facebook-domain-verification" content="8zs585g7z8634upvdctyplyk3g5w5l" />
       </Helmet>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
